0a3022c3d87998de49c8585c411903087234eb73
pong-nb <sompong@edispro.com>
2018-12-21 11:18:05 +0700

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,13 @@ set -e
 USERNAME=${USERNAME:-rsync}
 PASSWORD=${PASSWORD:-rsync}
   echo "root:$PASSWORD" | chpasswd
-  echo "$USERNAME:$PASSWORD" | chpasswd
+  cat /etc/passwd | grep ${USER_NAME} >/dev/null 2>&1
+if [ $? -eq 0 ] ; then
+echo "${USERNAME}:${PASSWORD}" | chpasswd -c SHA512
+else
+adduser $USERNAME --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+echo "${USERNAME}:${PASSWORD}" | chpasswd -c SHA512
+fi
  mkdir -p $VOLUME
  chown -R ${USERNAME}:${USERNAME} $VOLUME
 usermod -d $VOLUME -s /bin/bash ${USERNAME}
@@ -13,6 +19,6 @@ usermod -d $VOLUME -s /bin/bash ${USERNAME}
         chmod 400 /root/.ssh/authorized_keys
         chown root:root /root/.ssh/authorized_keys
     fi
-    exec /usr/sbin/sshd &
+    exec /usr/sbin/sshd
 
 exec "$@"
